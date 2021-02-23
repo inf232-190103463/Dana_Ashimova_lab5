24f0146ab2c0f724c72452b61cca3890fa6a9e8f
Dana Ashimova <79459335+inf232-190103463@users.noreply.github.com>
2021-02-23 13:13:36 +0600

    Add files via upload

---
+++ student.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab</title>
+</head>
+<body>
+    <div style="text-align: center; font-size:150px;">Dana Ashimova</div>
+    
+    <h1>Name: </h1>
+    <?php
+    echo $name;
+    ?>
+    <h1>Age: </h1>
+    <?php
+    echo $age;
+    ?>    
+    <h1>Date: </h1>
+    <?php
+    echo $date;
+    ?>
+</body>
+</html>
